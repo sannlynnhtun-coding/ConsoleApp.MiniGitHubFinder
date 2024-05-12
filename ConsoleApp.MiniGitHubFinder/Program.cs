8f3f31efc6aa7f4e6f7345828a46f99841e3dc72
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-12 23:51:07 +0630

    [Complete] Add Dumpify

--- ConsoleApp.MiniGitHubFinder/Program.cs
+++ ConsoleApp.MiniGitHubFinder/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using Dumpify;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 // Initialize HttpClient
@@ -19,19 +20,31 @@ if (user is null)
     Console.WriteLine("User not found.");
     return;
 }
-Console.WriteLine($"Username: {user.Login}");
-Console.WriteLine($"Name: {user.Name}");
-Console.WriteLine($"Location: {user.Location}");
-Console.WriteLine($"Followers: {user.Followers}");
-Console.WriteLine($"Following: {user.Following}");
+//Console.WriteLine($"Username: {user.Login}");
+//Console.WriteLine($"Name: {user.Name}");
+//Console.WriteLine($"Location: {user.Location}");
+//Console.WriteLine($"Followers: {user.Followers}");
+//Console.WriteLine($"Following: {user.Following}");
+new
+{
+    UserName = user.Login,
+    Name = user.Name,
+    Location = user.Location,
+    Followers = user.Followers,
+    Following = user.Following,
+}.Dump();
 
 // Fetch repositories
 var reposResponse = await client.GetAsync($"https://api.github.com/users/{username}/repos");
 var repositories = await reposResponse.Content.ReadFromJsonAsync<Repository[]>();
 
 Console.WriteLine("Repositories:");
-foreach (var repo in repositories!)
-{
-    Console.WriteLine($"{repo.Name}: {repo.Description}");
-}
+//foreach (var repo in repositories!)
+//{
+//    Console.WriteLine($"{repo.Name}: {repo.Description}");
+//}
+
+string[] lst = repositories!.Select(x => x.Name).ToArray();
+lst.Dump();
+
 goto result;
